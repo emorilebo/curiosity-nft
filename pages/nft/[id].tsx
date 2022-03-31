e13e7bee7bf9396e0f629388982a1e1bd6359887
Godfrey Lebo <emorylebo@gmail.com>
2022-03-31 19:42:23 +0100

    Right header implementation

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -4,7 +4,7 @@ function NFTDropPage() {
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/* Left */}
-      <div className="lg:col-span-4 bg-gradient-to-br from-cyan-800 to-rose-500">
+      <div className="bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4">
         <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
           <div className="rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2">
             <img
@@ -13,7 +13,7 @@ function NFTDropPage() {
               alt=""
             />
           </div>
-          <div className='space-y-2 p-5 text-center'>
+          <div className="space-y-2 p-5 text-center">
             <h1 className="text-4xl font-bold text-white">Xplorer Apes</h1>
             <h2 className="text-xl text-gray-300">
               A collection of apes who are trying to explore the vast space of
@@ -23,14 +23,25 @@ function NFTDropPage() {
         </div>
       </div>
       {/* Right */}
-      <div>
-        <div>
-          {/* Header */}
+      <div className="flex flex-1 flex-col p-12 lg:col-span-6">
+        {/* Header */}
+        <header className="flex items-center justify-between">
+          <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+            The{' '}
+            <span className="font-extrabold underline decoration-pink-600/50">
+              {' '}
+              CuriosityXplorer
+            </span>{' '}
+            NFT Market Place
+          </h1>
+          <button className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base">
+            Sign In
+          </button>
+        </header>
+        <hr className="my-2 border" />
+        {/* Content */}
 
-          {/* Content */}
-
-          {/* Mint Button */}
-        </div>
+        {/* Mint Button */}
       </div>
     </div>
   )
