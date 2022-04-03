f3ed75db815cdf1cee5d2effebfd8a96b7f4857f
Godfrey Lebo <emorylebo@gmail.com>
2022-04-03 13:48:31 +0100

    ADD thirdweb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,60 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Search from './search'
+import { sanityClient, urlFor } from '../sanity'
+import { Collection } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[]
+}
+
+const Home = ({ collections }: Props) => {
   return (
     <div className="">
       <Head>
         <title>CURIOUS NFT DROP</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Search/>
-      
+      <Search />
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `*[_type == "collection"]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+    asset
+  },
+  previewImage {
+    asset
+  },
+  slug {
+    current
+  },
+  creator -> {
+    _id,
+    name,
+    address,
+    slug {
+    current
+  },
+  },
+  }`
+
+  const collections = await sanityClient.fetch(query)
+  console.log(collections)
+
+  return {
+    props: {
+      collections,
+    },
+  }
+}
